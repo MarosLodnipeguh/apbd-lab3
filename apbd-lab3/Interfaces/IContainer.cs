6a0bb4c8bf823d83e1d3bbbb828f5b11863e5a48
s28781 <s28781@pjwstk.edu.pl>
2024-03-19 13:21:42 +0100

    tutorial

---
+++ apbd-lab3/Interfaces/IContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace apbd_lab3.Interfaces;
+
+public interface IContainer
+{
+    public double CargoWeight { set; get; }
+    public double Height { set; get; }
+}
--- apbd-lab3/Program.cs
+++ apbd-lab3/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using apbd_lab3;
 
 Console.WriteLine("Hello, World!");
+
+Container c = new Container(10, 15);
+
+
+var numbers = new List<int>() {1,2,3,4};
+
+Dictionary<PossibleProducts, double> products = new();
